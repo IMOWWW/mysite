63c501355bc3ce54c5cdc8c169d9772564a39327
IMOWWW <imowww@yandex.ru>
2024-06-22 18:10:50 +0500

    add urls reverse and  paginations

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 
@@ -16,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250) 
-    slug = models.SlugField(max_length=250) 
+    slug = models.SlugField(max_length=250, unique_for_date='publish') 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -38,3 +39,5 @@ class Post(models.Model):
         return self.title
 
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post = Post.published.all()
+    
+    paginator = Paginator(post, 2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     context = {
         'posts': posts,
         }
@@ -13,8 +19,8 @@ def post_list(request):
 
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED) 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day) 
     context = {
         'post': post
         }
