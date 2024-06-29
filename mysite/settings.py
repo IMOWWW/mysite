2ecef4d02fd3e803a22423b1791dfb104ccf8800
Илья Стародубцев <imowww@Ilas-MacBook-Pro.local>
2024-06-29 19:16:18 +0500

    fgdhj

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'imowww1234@gmail.com'
-EMAIL_HOST_PASSWORD = 'dszfxgchvjbknlm,kjhvgcfxd'
+EMAIL_HOST_PASSWORD = 'fgthryjdyhgrfed'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
